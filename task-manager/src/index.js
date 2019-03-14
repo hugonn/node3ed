19ffa3db005a08692a1cf36eb27d76e357a80ceb
HugoKeD <hugo.giardini@ked.com.br>
2019-03-14 15:39:11 -0300

    up

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/user')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -15,6 +16,57 @@ app.post('/users', (req,res) =>{
     user.save().then(() =>{
         console.log("foi")
         res.send(user)
+
+    }).catch((e)=>{
+        res.status(400).send(e)
+    })
+})
+
+app.get('/users/:id',(req,res) => {
+
+    const _id =  req.params.id
+    User.findById(_id).then((user)=>{
+        if(!user){
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }).catch(() => {
+        return res.status(500).send() 
+    })
+})
+
+app.get('/tasks',(req,res) => {
+
+    Task.find({}).then((tasks) =>{
+        res.send(tasks)
+    }).catch((e)=>{
+        res.status(500).send(e)
+    })
+})
+
+
+app.get('/tasks/:id',(req,res) => {
+
+    const _id =  req.params.id
+    Task.findById(_id).then((task)=>{
+        if(!task){
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    }).catch(() => {
+        return res.status(500).send() 
+    })
+})
+
+app.post('/tasks', (req,res) =>{
+    const model = new Model(req.body)
+    console.log(model)
+    
+    model.save().then(() =>{
+        console.log("foi")
+        res.send(model)
 
     }).catch((e)=>{
         res.status(400).send(e)
